aad0bf24c1e3b8666ede4b44ca327dde49e8ca6c
Pierr <pierre.besson7@gmail.com>
2012-06-16 11:08:58 +0200

    Initial commit for master_kine application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MasterKine::Application.config.secret_token = 'fe54a299867938217f5ef5b0fd2eee9766b96992636db2e3d3acb32c6663b3afe5e0e53a6143063a5c74f975dd9a0e639f9035a54f98e5d745b2a37d7208f77d'
